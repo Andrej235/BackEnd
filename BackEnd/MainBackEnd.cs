91cc1adc7c66fe628d34ffc7cab7fe4ebf1d7d87
Andrej235 <andrejnana445@gmail.com>
2023-06-25 00:11:46 +0200

    Initial commit

---
+++ BackEnd/MainBackEnd.cs
@@ -0,0 +1,18 @@
+﻿namespace BackEnd
+{
+    using BackEnd.Data;
+    using BackEnd.Models;
+    public class MainBackEnd
+    {
+        static SomethingContext Context = null!;
+        public static void Start()
+        {
+            Context = new();
+        }
+
+        public static List<Product> GetAllProducts()
+        {
+            return Context.Product.ToList();
+        }
+    }
+}
---
+++ BackEnd/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace BackEnd.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
